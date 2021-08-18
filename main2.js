284cfe97c91b001b03ccbfc30ada6d3a01bf1f54
ignapimo <ignapimoar@gmail.com>
2021-08-18 13:29:23 -0300

    JS

---
+++ main2.js
@@ -0,0 +1,20 @@
+let numero = parseInt(prompt("Ingresa un numero"))
+
+if (numero >"1000") {
+    alert ("Numero es superior a 1000")
+}
+
+let texto = (prompt("Ingresa un saludo"))
+
+if (texto=="hola") {
+    console.log ("Hola!!!1")   
+}
+
+let numero2 = parseInt(prompt("Ingresa un numero"))
+
+if (numero2 >="10" & numero2 <="50") {
+    alert ("Numero está entre parametros")
+  } else {
+      alert ("Numero no esta dentro de los parametros")
+  }
+  
