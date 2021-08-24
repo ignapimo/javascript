75be4740dfb5add2d082bf5ed2cc14284022dc67
ignapimo <ignapimoar@gmail.com>
2021-08-24 20:29:08 -0300

    clase3

--- main2.js
+++ main2.js
@@ -1,20 +1,126 @@
-let numero = parseInt(prompt("Ingresa un numero"))
 
-if (numero >"1000") {
-    alert ("Numero es superior a 1000")
+
+let notaAcumulada = 0;
+let contador = 0;
+                                                
+do {
+    var valor = parseInt(prompt("ingrese una nota"))
+    if (valor <0 || valor >10 ){
+        alert ("por favor ingresar un numero del 0 al 10")
+    }
+    notaAcumulada += valor;
+    contador++;
+
+    var sigo = prompt("Desea agregar mas notas")
+
+
+} while ((valor >=0 && valor <=10) && (sigo == "si"))
+
+
+let promedio = notaAcumulada/ contador;
+
+if (promedio <6){
+    console.log ("estas desaprobado")
+} else if ( promedio >=6 && promedio < 8) {
+    console.log ("estas aprobado")
+} else if ( promedio >= 8 && promedio < 10){
+    console.log ("Promocionado")
+} else {
+    console.log ("estas aprobado")
 }
 
-let texto = (prompt("Ingresa un saludo"))
 
-if (texto=="hola") {
-    console.log ("Hola!!!1")   
+
+
+
+let notasAcumuladas = 0
+let cantidadNotas = parseInt(prompt("ingresa el numero de notas"))
+
+for (let i = 0; i < cantidadNotas ; i++) {
+    let nota = parseInt(prompt("ingrese nota"))
+    notasAcumuladas += nota;
+
 }
 
-let numero2 = parseInt(prompt("Ingresa un numero"))
+let promedio = notasAcumuladas/ cantidadNotas;
 
-if (numero2 >="10" & numero2 <="50") {
-    alert ("Numero está entre parametros")
-  } else {
-      alert ("Numero no esta dentro de los parametros")
-  }
-  
+if (promedio <6){
+    console.log ("estas desaprobado")
+} else if ( promedio >=6 && promedio < 8) {
+    console.log ("estas aprobado")
+} else if ( promedio >= 8 && promedio < 10){
+    console.log ("Promocionado")
+} else {
+    console.log ("estas aprobado")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+for (let i=0; i <10 ; i++) {
+    console.log (i)
+    i =i+2
+}
+
+*/
+
+/*
+
+for (let i=0; i<10; i++) {
+    console.log (i)
+    if (i==5) {
+        break
+    }
+}
+console.log ("terminamos")
+*/
+
+/*
+for (let i = 0; i <10; i++) {
+    for ( let j = 0 ; j<5; j++) {
+        console.log ("Bienvenidos/as")
+    }
+}
+*/
+
+/*
+let valor = Number(prompt("ingrese un valorrr"))
+
+while (valor !=1) {
+    console.log(valor)
+    valor =Number(prompt("ingrese un valor"))
+
+}
+*/
