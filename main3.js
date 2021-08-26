59cd60c031938be02d1884d99dcf17becbd5cc8a
ignapimo <ignapimoar@gmail.com>
2021-08-26 00:00:46 -0300

    08/26

---
+++ main3.js
@@ -0,0 +1,202 @@
+function multiplicacion(num1, num2) {
+    return num1 * num2
+}
+
+
+
+function solicitarNumero() {
+    let numero = Number(prompt("Ingrese un numero"))
+    return numero
+}
+
+let numero1 = solicitarNumero();
+let numero2 = 0.21;
+let resultadoMultiplicacion = multiplicacion(numero1, numero2);
+
+
+console.log (resultadoMultiplicacion + numero1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let nombre1 = prompt("ingrese un nombre de un alumno/a")
+console.log (nombre1);
+let nombre2 = prompt("ingrese un nombre de un alumno/a")
+console.log (nombre2);
+let nombre3 = prompt("ingrese un nombre de un alumno/a")
+console.log (nombre3);
+let nombre4 = prompt("ingrese un nombre de un alumno/a")
+console.log (nombre4);
+let nombre5 = prompt("ingrese un nombre de un alumno/a")
+console.log (nombre5);
+let nombre6 = prompt("ingrese un nombre de un alumno/a")
+console.log (nombre6);
+*/
+
+/*
+function pedirnombre() {
+    let nombre = prompt ("ingrese nombre")
+    console.log (nombre);
+}
+
+pedirnombre();
+pedirnombre();
+pedirnombre();
+pedirnombre();
+pedirnombre();
+pedirnombre();
+
+*/
+
+/*
+function suma(num1, num2) {
+    return num1 + num2
+}
+
+function solicitarNumero() {
+    let numero = Number(prompt("Ingrese un numero"))
+    return numero
+}
+
+let numero1 = solicitarNumero();
+let numero2 = solicitarNumero();
+let resultado = suma(numero1, numero2)
+
+console.log (resultado)
+*/
+
+/*
+
+function multiplicacion(num1, num2) {
+    return num1 * num2
+}
+
+function suma(num1, num2) {
+    return num1 + num2
+}
+
+function resta(num1, num2) {
+    return num1 - num2
+}
+
+function division(num1, num2) {
+    return num1 / num2
+}
+
+function solicitarNumero() {
+    let numero = Number(prompt("Ingrese un numero"))
+    return numero
+}
+
+let numero1 = solicitarNumero();
+let numero2 = solicitarNumero();
+let resultadoSuma = suma(numero1, numero2)
+let resultadoMultiplicacion = multiplicacion(numero1, numero2)
+let resultadoDivision = division(numero1, numero2)
+let resultadoResta = resta(numero1, numero2)
+
+console.log ( resultadoSuma)
+console.log ( resultadoMultiplicacion)
+console.log ( resultadoDivision)
+console.log ( resultadoResta)
+*/
+
+
+/*
+
+function calculator(numero1, numero2, operacion) {
+    switch (operacion) {
+        case "+":
+            return numero1 + numero2;
+        case "-":
+            return numero1 - numero2;
+        case "*":
+            return numero1 * numero2;
+        case "/":
+            return numero1 / numero2;
+        default:
+            return 0;                           
+
+    }
+    
+}
+
+console.log(calculator(10,5,"*"))
+
+*/
+
+
+/*
+let suma = (numero1, numero2) => numero1 + numero2;
+let multiplicacion = (numero1, numero2) => numero1 * numero2;
+let division = (numero1, numero2) => numero1 / numero2;
+let resta = (numero1, numero2) => numero1 - numero2;
+
+console.log(suma(5,6));
+console.log(multiplicacion(5,6));
+console.log(division(5,6));
+console.log(resta(5,6));
+
+*/
+
+/*
+function suma(num1, num2) {
+    return num1 + num2
+}
+
+function solicitarNumero() {
+    let numero = Number(prompt("Ingrese un numero"))
+    return numero
+}
+
+let numero1 = solicitarNumero();
+let numero2 = solicitarNumero();
+let resultado = suma(numero1, numero2)
+
+console.log (resultado)
+*/
+
+
+
